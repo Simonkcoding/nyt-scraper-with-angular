6c161cf214749cb955e69d20c6c28ee2b69513ed
simonkcoding <ykwok3@uncc.edu>
2018-12-23 18:37:36 -0500

    123

--- server.js
+++ server.js
@@ -12,16 +12,9 @@ app.use('/api/articles', articles);
 
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', function (req, res, next) {
-    res.sendFile('dist/frontend/index.html', { root: __dirname });
-    });
-
-// // if (process.env.NODE_ENV === 'production'){
-//     app.use(express.static(__dirname + './frontend/dist/frontend'));
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, 'frontend', 'dist', 'frontend','index.html'));
-//       });
-// // };
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
+});
 
 const db = require('./config/key').mongoURI;
 
