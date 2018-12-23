5ccac261e34521fc04981f9534d5c609fe311141
simonkcoding <ykwok3@uncc.edu>
2018-12-23 13:29:21 -0500

    213

--- server.js
+++ server.js
@@ -8,20 +8,27 @@ var path = require("path");
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api/articles',articles);
+app.use('/api/articles', articles);
 
-// if (process.env.NODE_ENV === 'production'){
-    app.use(express.static(__dirname + './frontend/dist/frontend'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'dist', 'frontend','index.html'));
-      });
-// };
+
+app.use('frontend/dist/frontend', express.static(path.join(__dirname, '/')));
+// Handle React routing, return all requests to React app
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'frontend/dist/frontend', 'index.html'));
+});
+
+// // if (process.env.NODE_ENV === 'production'){
+//     app.use(express.static(__dirname + './frontend/dist/frontend'));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'frontend', 'dist', 'frontend','index.html'));
+//       });
+// // };
 
 const db = require('./config/key').mongoURI;
 
 mongoose.connect(db)
-.then(()=>console.log('connected mongoDB'))
-.catch(err=>console.log(err));
+    .then(() => console.log('connected mongoDB'))
+    .catch(err => console.log(err));
 
-const port = process.env.PORT||5000;
-app.listen(port,()=>console.log(`server started at ${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server started at ${port}`));
