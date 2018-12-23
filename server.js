efe4afaf0b5d716ae8000efcbd5cbd2bd5903bb8
simonkcoding <ykwok3@uncc.edu>
2018-12-23 13:18:10 -0500

    123

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(cors());
 app.use('/api/articles',articles);
 
 // if (process.env.NODE_ENV === 'production'){
-    app.use(express.static(__dirname + './frontend/dist'));
+    app.use(express.static(__dirname + './frontend/dist/frontend'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname,'index.html'));
+        res.sendFile(path.join(__dirname, 'frontend', 'dist', 'frontend','index.html'));
       });
 // };
 
