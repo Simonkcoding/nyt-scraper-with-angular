36c5a6d15c8a9fcda216cc559ea1eea0be267d74
simonkcoding <ykwok3@uncc.edu>
2018-12-23 11:11:08 -0500

    path2

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ app.use(cors());
 app.use('/api/articles',articles);
 
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public/index.html'));
+      });
+      
 };
 
 const db = require('./config/key').mongoURI;
